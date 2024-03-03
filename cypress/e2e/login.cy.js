5a7bd24d36a5987112cf8d7474a270e2331972fe
Larissa-Polhmann <lari.polhmann@gmail.com>
2024-03-02 22:15:50 -0300

    Fix login bug

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,13 +2,15 @@
 import { loc } from '../support/locators'
 
 describe('Login', () => {
+	beforeEach(() => {
+		cy.visit('/')
+	})
 
 	it('Successfully logs in', () => {
 		cy.login()
 	})
 
 	it('Locked out user', () => {
-		cy.visit('/')
 		cy.get(loc.LOGIN.USER).type('locked_out_user')
 		cy.get(loc.LOGIN.PASSWORD).type(Cypress.env('USER_PASSWORD'))
 		cy.get(loc.LOGIN.LOGIN_BTN).click()
